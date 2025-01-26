61fe4d4824c9c1f4326b4e12e07324f658f6a221
uchakravarthi2754 <uchakravarthi2754@gmail.com>
2025-01-26 01:19:42 -0600

    Create python script_name.py

---
+++ python script_name.py
@@ -0,0 +1,7 @@
+echo "# python-practice" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/uchakravarthi2754/python-practice.git
+git push -u origin main
